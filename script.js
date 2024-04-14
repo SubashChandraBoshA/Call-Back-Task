7fce459ae5f7bc0d0812d373a511ec842c071af8
SubashChandraBoshA <160812687+SubashChandraBoshA@users.noreply.github.com>
2024-04-14 23:04:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout (() => {
+document.getElementById ("display").innerText = "10";
+setTimeout (() => {
+    document.getElementById ("display").innerText = "09";
+    setTimeout (() => {
+        document.getElementById ("display").innerText = "08";
+        setTimeout (() => {
+            document.getElementById ("display").innerText = "07";
+            setTimeout (() => {
+                document.getElementById ("display").innerText = "06";
+                setTimeout (() => {
+                    document.getElementById ("display").innerText = "05";
+                    setTimeout (() => {
+                        document.getElementById ("display").innerText = "04";
+                        setTimeout (() => {
+                            document.getElementById ("display").innerText = "03";
+                            setTimeout (() => {
+                                document.getElementById ("display").innerText = "02";
+                                setTimeout (() => {
+                                    document.getElementById ("display").innerText = "01";
+                                    setTimeout(()=>{
+                                        document.getElementById("display").innerText= "***Happy Independence Day***";
+                                    }, 1000);
+},1000);
+},1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+},1000);
